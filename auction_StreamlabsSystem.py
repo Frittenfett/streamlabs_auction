2240e5b8b0a6f9be1a665b3c59aaa9d548a7bfed
Frittenfett <ruv_F@web.de>
2018-07-05 00:41:12 +0200

    Core code... does not work yet, but close :D

--- auction_StreamlabsSystem.py
+++ auction_StreamlabsSystem.py
@@ -18,7 +18,7 @@ ScriptName = "Auction"
 Website = "https://www.twitch.tv/frittenfettsenpai"
 Description = "Auction System. Right click and add api key!"
 Creator = "frittenfettsenpai"
-Version = "0.0.2"
+Version = "0.0.3"
 
 # ---------------------------------------
 #   [Required] Intialize Data (Only called on Load)
@@ -35,12 +35,18 @@ def Init():
             "commandBid": "!bid",
             "commandStartBid": "!startBid",
             "commandStopBid": "!stopBid",
+            "finalCountdownStart": 10,
+            "defaultCountdown": 120,
             "languageBidNotRunning": "An active bid is currently not running!",
             "languageBidAlreadyStarted": "Bid is already in progress. Stop the bid first!",
-            "languageBidNoCooldownParam": "Please use the correct syntax: {0} <1-999 seconds>",
-            "languageBidGameStarted": "{0} has opend an auction. Type {1} <1-9999> to place a bid!",
+            "languageBidGameStarted": "{0} has opened an auction for {1} seconds. Type {2} <1-9999> to place a bid!",
             "languageBidGameClosed": "{0} has closed the auction.",
-            "languageNewBidLeader": "{0} is now leading with {1} {2}."
+            "languageNewBidLeader": "{0} is now leading with {1} {2}.",
+            "languageCountdownNobody": "{0} seconds left... Nobody did a bid yet.",
+            "languageCountdownNormal": "{0} seconds left... {1} is on the lead with {2} {3}.",
+            "languageCountdownFinal": "Last {0} seconds!! {1} is leading with {2} {3}.",
+            "languageWon": "{0} has won this auction with a bid of {1} {2}. Congratulations.",
+            "languageNobodyWon": "Nobody did a bid. Sadly, nobody wins :(",
         }
     bidMaxAmount = 0
     bidEnabled = 0
@@ -57,33 +63,29 @@ def Execute(data):
     if data.IsChatMessage():
         user = data.User
         username = Parent.GetDisplayName(user)
-        if (bidEnabled == 1 and data.GetParam(0).lower() == settings["commandBid"] and data.GetParamCount() > 1):
+        if (bidEnabled == 1 and data.GetParam(0) == settings["commandBid"] and data.GetParamCount() > 1):
             userBid = int(data.GetParam(1))
-            if (Parent.GetPoints(user) > userBid and userBid > bidMaxAmount):
+            if (Parent.GetPoints(user) >= userBid and userBid > bidMaxAmount):
                 bidMaxAmount = userBid
                 bidMaxUser = user
+                if activeFor < settings["finalCountdownStart"]:
+                    activeFor = settings["finalCountdownStart"]
                 Parent.SendTwitchMessage(settings["languageNewBidLeader"].format(username, bidMaxAmount, Parent.GetCurrencyName()))
-        elif (data.GetParam(0).lower() == settings["commandStartBid"]):
+        elif (data.GetParam(0) == settings["commandStartBid"]):
             if (bidEnabled == 1):
                 Parent.SendStreamWhisper(user, settings["languageBidAlreadyStarted"])
             else:
-                if data.GetParamCount() <= 1:
-                    Parent.SendStreamWhisper(user, settings["languageBidNoCooldownParam"].format(settings["commandStartBid"]))
-                else:
-                    bidEnabled = 1
-                    bidMaxAmount = 0
-                    bidMaxUser = ""
+                if data.GetParamCount() > 1:
                     activeFor = int(data.GetParam(1))
-                    Parent.SendTwitchMessage(settings["languageBidGameStarted"].format(user, settings["commandBid"]))
-                    start_game()
-        elif (data.GetParam(0).lower() == settings["commandStopBid"]):
+                else:
+                    activeFor = settings["defaultCountdown"]
+                Parent.SendTwitchMessage(settings["languageBidGameStarted"].format(user, activeFor, settings["commandBid"]))
+                start_game()
+        elif (data.GetParam(0) == settings["commandStopBid"]):
             if (bidEnabled == 0):
                 Parent.SendStreamWhisper(user, settings["languageBidNotRunning"])
             else:
-                bidEnabled = 0
-                bidMaxAmount = 0
-                activeFor = 0
-                bidMaxUser = ""
+                reset_game()
                 Parent.SendTwitchMessage(settings["languageBidGameClosed"].format(username))
     return
 
@@ -97,9 +99,43 @@ def Tick():
 
 
 def start_game():
+    global settings, bidMaxAmount, bidMaxUser, bidEnabled, activeFor
+    bidEnabled = 1
+    bidMaxAmount = 0
+    bidMaxUser = ""
+    while activeFor > 0 and bidEnabled == 1:
+        if activeFor == 30 or activeFor == 60 or activeFor == settings["finalCountdownStart"]:
+            if bidMaxUser != "":
+                bitMaxUsername = Parent.GetDisplayName(bidMaxUser)
+                if activeFor == settings["finalCountdownStart"]:
+                    Parent.SendTwitchMessage(settings["languageCountdownFinal"].format(activeFor, bitMaxUsername, bidMaxAmount,Parent.GetCurrencyName()))
+                else:
+                    Parent.SendTwitchMessage(settings["languageCountdownNormal"].format(activeFor, bitMaxUsername, bidMaxAmount, Parent.GetCurrencyName()))
+            else:
+                Parent.SendTwitchMessage(settings["languageCountdownNobody"].format(activeFor))
+        time.sleep(1)
+        activeFor = activeFor - 1
+    if bidEnabled == 1:
+        end_game()
     return
+
 
 def end_game():
-    # Parent.RemovePoints(user, settings["costs"])
+    global settings, bidMaxAmount, bidMaxUser, bidEnabled, activeFor
+    if bidMaxUser != "":
+        Parent.RemovePoints(bidMaxUser, bidMaxAmount)
+        bitMaxUsername = Parent.GetDisplayName(bidMaxUser)
+        Parent.SendTwitchMessage(settings["languageWon"].format(bitMaxUsername, bidMaxAmount, Parent.GetCurrencyName()))
+        reset_game()
+    else:
+        Parent.SendTwitchMessage(settings["languageNobodyWon"])
     return
 
+
+def reset_game():
+    global bidMaxAmount, bidMaxUser, bidEnabled, activeFor
+    bidEnabled = 0
+    bidMaxAmount = 0
+    activeFor = 0
+    bidMaxUser = ""
+    return
